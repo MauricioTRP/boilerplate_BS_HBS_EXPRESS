7a19f41842a702aef424806bfa81c54f7696c137
Mauricio Fuentes <mauriciofb@gmail.com>
2024-04-28 22:34:42 -0400

    Basic boilerplate

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000")
+})
+
+app.set("view engine", "handlebars")
+
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    layoutsDir: __dirname + '/views'
+  })
+)
+
+app.use("/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist'))
+app.use("/popper", express.static(__dirname + "/node_modules/@popperjs/core/dist/umd"))
+
+app.get('/', (req, res) => {
+  res.render("dashboard")
+})
+
